9f9b9a782605017e09db1ac39c11741a860c23fc
IT24100850 <it24100850@my.sliit.lk>
2024-08-17 15:37:39 +0530

    Add files via upload

---
+++ Q1/Lab4/IT24100850Lab4Q1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class IT24100850Lab4Q1
+{
+      public static void main(String [] args)
+      {
+      
+        Scanner input = new Scanner(System.in);
+       
+        System.out.print("Enter a number:");
+        int number = input.nextInt();
+  
+        if (number > 0)
+        {
+       
+          System.out.println(" The number is positive:");
+        }
+
+        else if (number < 0)
+        {
+       
+          System.out.println(" The number is negative:");
+        }
+ 
+        else 
+        {
+     
+          System.out.println(" The number is Zero");
+        }
+
+      }
+}
+    
+         
